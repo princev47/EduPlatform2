e6c51b23fab97c64ef67210ac74c8f08dc9071db
Prince Singh <most.lyprince2004@gmmail.com>
2025-10-27 03:58:49 +0530

    backend change

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://eduplatform-j4j9.onrender.com',
+  baseURL: 'https://eduplatform2.onrender.com',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json'
