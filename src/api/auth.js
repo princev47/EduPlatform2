cd958a7b3202c2446699b274224fec53ef4aa258
Prince Singh <most.lyprince2004@gmmail.com>
2025-10-30 01:25:37 +0530

    basic

--- src/api/auth.js
+++ src/api/auth.js
@@ -2,5 +2,5 @@ import api from './api'
 
 export const sendOtp = (email) => api.post('/api/v1/user/sendotp', { email })
 export const signup = (payload) => api.post('/api/v1/user/signup', payload)
-export const login = (payload) => api.post('/api/v1/user//login', payload)
+export const login = (payload) => api.post('/api/v1/user/login', payload)
 export const getProfile = () => api.get('/api/v1/user/get') 
